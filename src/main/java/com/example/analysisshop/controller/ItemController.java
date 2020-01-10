859ff0ebb45a49bf9637b8c3c010dd0d21a2da4d
ming <mingchungchan@163.com>
2020-01-10 18:06:30 +0800

    init ControllerHandler,redis

--- src/main/java/com/example/analysisshop/controller/ItemController.java
+++ src/main/java/com/example/analysisshop/controller/ItemController.java
@@ -3,9 +3,12 @@ package com.example.analysisshop.controller;
 import com.example.analysisshop.common.AssertUtil;
 import com.example.analysisshop.common.Result;
 import com.example.analysisshop.common.ResultUtils;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/shop")
@@ -19,11 +22,21 @@ public class ItemController {
     }
 
 
+
+
+
     @ResponseBody
     @RequestMapping("/getMe")
     public Result getMe(Integer i) {
         System.out.println(i);
         AssertUtil.validIsNull(i);
+        return ResultUtils.succeed();
+    }
+
+    @ResponseBody
+    @RequestMapping("/testRedis")
+    public Result testRedis(HttpServletRequest request) {
+        request.getSession().setAttribute("ni","meiyou");
         return ResultUtils.succeed();
     }
 }
